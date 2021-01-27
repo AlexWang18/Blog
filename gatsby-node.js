039e5bac692dfd6ec36ea440b514d229839b499f
Alex Wang <alw245@pitt.edu>
2021-01-26 19:19:15 -0500

    hardcoded paths made from Node api working

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,8 +1,41 @@
+const path = require("path")
 const { createFilePath } = require("gatsby-source-filesystem")
-
-exports.onCreateNode = ({ node, getNode }) => {
+exports.onCreateNode = ({ node, getNode, actions }) => {
   if (node.internal.type === `MarkdownRemark`) {
-    console.log(createFilePath({ node, getNode, basePath: `pages`}))
-    
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    const { createNodeField } = actions // only owner of node can modify it
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
   }
 }
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const result = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.fields.slug,
+      component: path.resolve(`./src/templates/blog-post.js`),
+      context: {
+        // passed to context is available in page queries as variables.
+        slug: node.fields.slug,
+      },
+    })
+  })
+}
+    
